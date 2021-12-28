1a997aee1181c6cb0e9e416d969f72d9d6b96caa
robi212 <robiansyah212@gmail.com>
2021-12-28 15:03:36 +0800

    landing page

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// $(.page-scroll a).on('click',function (event) {
+//     if(this.hash !=='') {
+//         event.preventDefault();
+//     }
+//     const has = this.hash;
+//     $('html,body').animate({
+//         scrollTop:$(hash).offset().top - 500
+//     },800);
+    
+// })
+
+// $('.page-scroll').on ('click') function(){
+//     var href = $(this).attr('href');
+//     var elementHref =$(href);
+
+// });
+
+// Paralax
+// $(window).scroll (function(){
+//     var wScroll = $(this).scrollTop();
+//     // console.log(wScroll);
+
+//     $('img').css ({
+//         'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+//     });
+// });
+
+
+
+
+
+
+
+
+
